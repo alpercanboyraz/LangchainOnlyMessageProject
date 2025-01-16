bd4c85568a9553615b458f3e4a4380f21ac83fba
alpercanboyraz <boyrazalpercan@gmail.com>
2025-01-17 00:34:17 +0300

    added Dynamic Prompt

--- onlymessage.py
+++ onlymessage.py
@@ -1,19 +1,43 @@
 from dotenv import load_dotenv
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage,SystemMessage
-
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 load_dotenv()
 
+
+#region Model name and creativity
+MODEL_NAME = "gpt-4"
+TEMPERATURE = 0.1
+
 model = ChatOpenAI(
-    model="gpt-4", temperature = 0.1)
+    model=MODEL_NAME,
+    temperature=TEMPERATURE
+)
+#endregion
 
-TRANSLATION_PROMPT = "Translate the following from English to Spanish"
+#region System and Human Prompt
+#SYSTEM_PROMPT = "Translate the following from English to Spanish"
+#HUMAN_PROMPT = "Hi!"
+#messages = [
+#   SystemMessage(content = TRANSLATION_PROMPT),
+#   HumanMessage(content = HUMAN_PROMPT)
+#]
+#endregion
 
-messages = [
-    SystemMessage(content = TRANSLATION_PROMPT),
-    HumanMessage(content = "Hi!")
-]
+#region DYNAMIC !!  System and Human Prompt
+system_prompt = "Translate the following into {language}"
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system",system_prompt) , ("user", "{text}")
+    ]
+)
+#endregion
+
+
+parser = StrOutputParser()
+chain = prompt_template | model | parser
 
 if __name__ == "__main__":
-    response = model.invoke(messages)
-    print(response)
+
+    print(chain.invoke({"language" : "italian", "text" : "Hello World"}))
