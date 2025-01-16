8035fb8d9bc5d23f73cdbc7ab03b562a44d41b86
alpercanboyraz <boyrazalpercan@gmail.com>
2025-01-16 23:47:18 +0300

    Update requirements.txt and added translate prompt

--- onlymessage.py
+++ onlymessage.py
@@ -1,4 +1,19 @@
 from dotenv import load_dotenv
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import HumanMessage,SystemMessage
 
 load_dotenv()
 
+model = ChatOpenAI(
+    model="gpt-4", temperature = 0.1)
+
+TRANSLATION_PROMPT = "Translate the following from English to Spanish"
+
+messages = [
+    SystemMessage(content = TRANSLATION_PROMPT),
+    HumanMessage(content = "Hi!")
+]
+
+if __name__ == "__main__":
+    response = model.invoke(messages)
+    print(response)
