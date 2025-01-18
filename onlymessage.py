54aedb5f7e0f7c7e0d808849001ddf604b87b665
alpercanboyraz <boyrazalpercan@gmail.com>
2025-01-18 23:39:00 +0300

    added langserve

--- onlymessage.py
+++ onlymessage.py
@@ -3,6 +3,9 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage,SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from fastapi import FastAPI
+from langserve import add_routes
+
 load_dotenv()
 
 
@@ -38,6 +41,19 @@ prompt_template = ChatPromptTemplate.from_messages(
 parser = StrOutputParser()
 chain = prompt_template | model | parser
 
+app = FastAPI(
+    title = "Translate BOT",
+    version = "1.0.0",
+    description = "Translation BOT"
+)
+
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
 if __name__ == "__main__":
 
-    print(chain.invoke({"language" : "italian", "text" : "Hello World"}))
+    import uvicorn
+    uvicorn.run(app, host="localhost" , port=8000)
